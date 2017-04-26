d432c88e56a0bb847627ad327a27fc9c18223264
Yordan Dimitrov <dadanchi@abv.bg>
2017-04-26 17:43:01 +0300

    New homework

---
+++ C#/High Quality Code 2/Code tuning and optimization/MathsComparisons/CompareAdvandecMaths/StartUp.cs
@@ -0,0 +1,118 @@
+﻿namespace CompareAdvancedMaths
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            const float FloatExample = 1f;
+            const double DoubleExample = 1;
+            const decimal DecimalExample = 1m;
+
+            // Implement it once with no point because the fisrt time it takes longer
+            var firstTryResults = PerformFullComparison(FloatExample);
+
+            var floatResults = PerformFullComparison(FloatExample);
+            var doubleResults = PerformFullComparison(DoubleExample);
+            var decimalResults = PerformFullComparison(DecimalExample);
+
+            Print(floatResults, "float");
+            Print(doubleResults, "double");
+            Print(decimalResults, "decimal");
+        }
+
+        private static double CompareSqrt<T>(T number)
+        {
+            var results = new List<TimeSpan>();
+            var timeCounter = new Stopwatch();
+            double doubleNumber = 1;
+
+            for (int i = 0; i < 10; i++)
+            {
+                timeCounter.Start();
+
+                for (int j = 0; j < 10000; j++)
+                {
+                    doubleNumber = Math.Sqrt((double)(dynamic)number);
+                }
+
+                timeCounter.Stop();
+                results.Add(timeCounter.Elapsed);
+
+                timeCounter.Reset();
+            }
+
+            return results.Average(ts => ts.Milliseconds);
+        }
+
+        private static double CompareNaturalLogarithm<T>(T number)
+        {
+            var results = new List<TimeSpan>();
+            var timeCounter = new Stopwatch();
+            double doubleNumber = 1;
+
+            for (int i = 0; i < 10; i++)
+            {
+                timeCounter.Start();
+
+                for (int j = 0; j < 10000; j++)
+                {
+                    doubleNumber = Math.Log((double)(dynamic)number);
+                }
+
+                timeCounter.Stop();
+                results.Add(timeCounter.Elapsed);
+
+                timeCounter.Reset();
+            }
+
+            return results.Average(ts => ts.Milliseconds);
+        }
+
+        private static double CompareSinus<T>(T number)
+        {
+            var results = new List<TimeSpan>();
+            var timeCounter = new Stopwatch();
+            double doubleNumber = 1;
+
+            for (int i = 0; i < 10; i++)
+            {
+                timeCounter.Start();
+
+                for (int j = 0; j < 10000; j++)
+                {
+                    doubleNumber = Math.Sin((double)(dynamic)number);
+                }
+
+                timeCounter.Stop();
+                results.Add(timeCounter.Elapsed);
+
+                timeCounter.Reset();
+            }
+
+            return results.Average(ts => ts.Milliseconds);
+        }
+
+        private static List<double> PerformFullComparison<T>(T number)
+        {
+            var results = new List<double>();
+            results.Add(CompareSqrt(number));
+            results.Add(CompareNaturalLogarithm(number));
+            results.Add(CompareSinus(number));
+
+            return results;
+        }
+
+        private static void Print(IList<double> collection, string type)
+        {
+            Console.WriteLine($"The average time for executing the Math.Sqrt for the {type} type is: {collection[0]}s");
+            Console.WriteLine($"The average time for executing the Math.Log for the {type} type is: {collection[1]}s");
+            Console.WriteLine($"The average time for executing the Math.Sin for the {type} type is: {collection[2]}s");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ C#/High Quality Code 2/Code tuning and optimization/MathsComparisons/CompareSimpleMaths/StartUp.cs
@@ -0,0 +1,173 @@
+﻿namespace CompareSimpleMaths
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            const int IntegerExample = 1;
+            const long LongExample = 1;
+            const float FloatExample = 1f;
+            const double DoubleExample = 1;
+            const decimal DecimalExample = 1m;
+
+            // Implement it once with no point because the fisrt time it takes longer
+            var firstTryResults = PerformFullComparison(FloatExample);
+
+            var intResults = PerformFullComparison(IntegerExample);
+            var longResults = PerformFullComparison(LongExample);
+            var floatResults = PerformFullComparison(FloatExample);
+            var doubleResults = PerformFullComparison(DoubleExample);
+            var decimalResults = PerformFullComparison(DecimalExample);
+
+            Print(intResults, "int");
+            Print(longResults, "long");
+            Print(floatResults, "float");
+            Print(doubleResults, "double");
+            Print(decimalResults, "decimal");
+        }
+
+        private static double CompareAdding<T>(T number)
+        {
+            var results = new List<TimeSpan>();
+            var timeCounter = new Stopwatch();
+
+            for (int j = 0; j < 10; j++)
+            {
+                timeCounter.Start();
+
+                for (int i = 0; i < 10000; i++)
+                {
+                    number += (dynamic)i;
+                }
+
+                timeCounter.Stop();
+                results.Add(timeCounter.Elapsed);
+
+                timeCounter.Reset();
+            }
+
+            return results.Average(ts => ts.Milliseconds);
+        }
+
+        private static double CompareSubstract<T>(T number)
+        {
+            var results = new List<TimeSpan>();
+            var timeCounter = new Stopwatch();
+
+            for (int j = 0; j < 10; j++)
+            {
+                timeCounter.Start();
+
+                for (int i = 0; i < 10000; i++)
+                {
+                    number -= (dynamic)i;
+                }
+
+                timeCounter.Stop();
+                results.Add(timeCounter.Elapsed);
+
+                timeCounter.Reset();
+            }
+
+            return results.Average(ts => ts.Milliseconds);
+        }
+
+        private static double CompareIncrementation(dynamic number)
+        {
+            var results = new List<TimeSpan>();
+            var timeCounter = new Stopwatch();
+
+            for (int j = 0; j < 10; j++)
+            {
+                timeCounter.Start();
+
+                for (int i = 0; i < 10000; i++)
+                {
+                    number++;
+                }
+
+                timeCounter.Stop();
+                results.Add(timeCounter.Elapsed);
+
+                timeCounter.Reset();
+            }
+
+            return results.Average(ts => ts.Milliseconds);
+        }
+
+        private static double CompareMultiplication<T>(T number)
+        {
+            var results = new List<TimeSpan>();
+            var timeCounter = new Stopwatch();
+
+            for (int j = 0; j < 10; j++)
+            {
+                timeCounter.Start();
+
+                for (int i = 0; i < 10000; i++)
+                {
+                    number *= (dynamic)i;
+                }
+
+                timeCounter.Stop();
+
+                results.Add(timeCounter.Elapsed);
+
+                timeCounter.Reset();
+            }
+
+            return results.Average(ts => ts.Milliseconds);
+        }
+
+        private static double CompareDivision<T>(T number)
+        {
+            var results = new List<TimeSpan>();
+            var timeCounter = new Stopwatch();
+
+            for (int j = 0; j < 10; j++)
+            {
+                timeCounter.Start();
+
+                for (int i = 1; i < 10000; i++)
+                {
+                    number /= (dynamic)i;
+                }
+
+                timeCounter.Stop();
+                results.Add(timeCounter.Elapsed);
+
+                timeCounter.Reset();
+            }
+
+            return results.Average(ts => ts.Milliseconds);
+        }
+
+        private static List<double> PerformFullComparison<T>(T number)
+        {
+            var results = new List<double>();
+            results.Add(CompareAdding(number));
+            results.Add(CompareSubstract(number));
+            results.Add(CompareIncrementation(number));
+            results.Add(CompareMultiplication(number));
+            results.Add(CompareDivision(number));
+
+            return results;
+        }
+
+        private static void Print(IList<double> collection, string type)
+        {
+            Console.WriteLine($"The average time for executing the adding for the {type} type is: {collection[0]}s");
+            Console.WriteLine($"The average time for executing the substracting for the {type} type is: {collection[1]}s");
+            Console.WriteLine($"The average time for executing the incremention for the {type} type is: {collection[2]}s");
+            Console.WriteLine($"The average time for executing the multiplication for the {type} type is: {collection[3]}s");
+            Console.WriteLine($"The average time for executing the division for the {type} type is: {collection[4]}s");
+            Console.WriteLine();
+        }
+    }
+}
+
